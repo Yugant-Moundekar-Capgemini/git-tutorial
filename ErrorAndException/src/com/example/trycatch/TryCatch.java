c15b8f895d357daf7b447f5f790f4aff7f51bb5f
Yugant-Moundekar-Capgemini <130477854+Yugant-Moundekar-Capgemini@users.noreply.github.com>
2023-04-30 21:28:56 +0530

    Added Exception java code in exception branch

---
+++ ErrorAndException/src/com/example/trycatch/TryCatch.java
@@ -0,0 +1,27 @@
+package com.example.trycatch;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 6000;
+        int b = 0;
+        // Without Try:
+//        int c = a / b;
+//        System.out.println("The result is " + c);
+        // With Try:
+        try {
+            int c = a / b;
+            System.out.println("The result is " + c);
+        }
+        catch(Exception e) {
+            System.out.println("We failed to divide. Reason: ");
+            System.out.println(e);
+        }
+        System.out.println("End of the program");
+		
+		
+	}
+
+}
---
+++ ErrorAndException/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ymoundek
+ *
+ */
+module ErrorAndException {
+}
